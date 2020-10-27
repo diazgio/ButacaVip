ef5c374bc4b67e59c9aad6c36fc89b5c19a70855
Giordano Dìaz <diazgiordano@gmail.com>
2020-10-27 16:12:53 -0500

    Create helper methods and style attendance

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,12 @@
 module EventsHelper
+  def are_attendees?
+    if current_user.id == @event.creator_id
+      countee = @event.attendees.each.count.positive?
+      content_tag :p, countee, :class => 'btn btn-warning p-3'
+    end
+  end
+
+  def creator?
+    current_user.id == @event.creator_id
+  end
 end
