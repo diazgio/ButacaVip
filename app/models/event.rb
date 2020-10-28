2e0e0f99a269b74bb9b84a848c10443e74204451
Giordano Dìaz <diazgiordano@gmail.com>
2020-10-28 11:39:53 -0500

    Add past events to Users profile

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   has_many :attendances
-  has_many :attendees, through: :attendances, source: :user
+  has_many :attendees, through: :attendances, source: :user, :dependent => :delete_all
   belongs_to :creator, class_name: "User", foreign_key: :creator_id
   scope :past_events, -> { where('end_date < ?', DateTime.now) }
   scope :future_events, -> { where('start_date > ?', DateTime.now) }
