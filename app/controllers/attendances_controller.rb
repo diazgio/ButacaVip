c1fe6ace4a827553e2bca67ed410cbe79043d0f6
Giordano Dìaz <diazgiordano@gmail.com>
2020-10-29 11:41:09 -0500

    Create dont attend botton in event show page

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,5 @@
 class AttendancesController < ApplicationController
+  
   def create
     @event = Event.find(params[:event_id])
     @event.attendees << current_user
@@ -10,4 +11,14 @@ class AttendancesController < ApplicationController
       redirect_to @event
     end
   end
+
+  def destroy
+    @attendace = Attendance.where(event_id: params[:event_id], user_id: current_user.id).first
+    @attendace.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, alert: 'Attendace was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
 end
