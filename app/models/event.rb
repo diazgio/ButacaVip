dbd98556de8cfc0e925fdb6704bcae8ef57e4a62
Giordano Dìaz <diazgiordano@gmail.com>
2020-10-28 19:35:41 -0500

    Stiky position on navbar

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,9 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: :creator_id
   scope :past_events, -> { where('end_date < ?', DateTime.now) }
   scope :future_events, -> { where('start_date > ?', DateTime.now) }
+  validates :location, presence: true, length: { minimum: 3, maximum: 255 }
+  validates :description, presence: true, length: { minimum: 3 }
+  validates :artist, presence: true, length: { minimum: 3, maximum: 255 }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
