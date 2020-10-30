a85c5801b5bfce29640f275a0235b2e8dfc21c44
Giordano Dìaz <diazgiordano@gmail.com>
2020-10-30 10:32:02 -0500

    Finish rspec test for models

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Attendace, type: :model do
+RSpec.describe Attendance, type: :model do
   describe 'associations' do
-    it { should belong_to(:event)
-    it { should belong_to(:user)
+    it { should belong_to(:event) }
+    it { should belong_to(:user) }
   end
 end
